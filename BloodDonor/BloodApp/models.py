dac6f953caba02a96c1ce2476dd02ede3bf99023
naveen <naveen@gmail.com>
2020-12-05 10:40:17 +0530

    jitendra is created model.py

--- BloodDonor/BloodApp/models.py
+++ BloodDonor/BloodApp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class EmployeeModel(models.Model):
+    idno=models.IntegerField(primary_key=True)
+    name=models.CharField(max_length=50)
+    salary=models.FloatField()
